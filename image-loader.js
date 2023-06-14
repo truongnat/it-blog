0a694088f3cd792f1e806b7cd362280214c5dce9
Truong.Nat <truongdq.dev@gmail.com>
2023-06-14 23:28:27 +0700

    feat: remove base url image loader

--- image-loader.js
+++ image-loader.js
@@ -1,5 +1,3 @@
 export default function myImageLoader({ src, width, quality }) {
-  return `${process.env.NEXT_PUBLIC_APP_URL}${src}?w=${width}&q=${
-    quality || 75
-  }`
+  return `${src}?w=${width}&q=${quality || 75}`
 }
